f8df5ced6393e99b2e90c53cd30a107b08d562ed
Dag Hjelm <dag.hjelm@gmail.com>
2022-09-06 09:48:36 +0200

    ftplugin for c

---
+++ ftplugin/c.lua
@@ -0,0 +1,6 @@
+local set_local = vim.opt_local
+
+set_local.tabstop = 4
+set_local.shiftwidth = 4
+set_local.expandtab = false
+
--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -46,4 +46,5 @@ require'lspconfig'.pyright.setup{}
 require'lspconfig'.tsserver.setup{}
 require'lspconfig'.gopls.setup{}
 require'lspconfig'.sumneko_lua.setup{}
+require'lspconfig'.clangd.setup{}
 require("luasnip.loaders.from_vscode").lazy_load()
